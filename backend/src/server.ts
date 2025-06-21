189e2f5085434ecccb769c7302c81cae5d83942d
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-06-21 20:51:24 +0800

    setup linter and husky

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
-import dotenv from "dotenv";
-import app from "./app";
+import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("server is running on port:", PORT);
+  console.log('server is running on port:', PORT);
 });
---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // Base JS rules
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  // TypeScript rules
+  ...tseslint.configs.recommended,
+
+  // React rules
+  {
+    files: ["**/*.{jsx,tsx}"],
+    plugins: {
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect", // auto-detects React version
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // ✅ fix for your error
+    },
+  },
+]);
