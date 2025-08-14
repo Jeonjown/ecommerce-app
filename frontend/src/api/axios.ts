ac69463c3d23dabaf0943742b90dcad3557b3a3e
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-08-14 19:55:02 +0800

    add prod dev option

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const isDev = import.meta.env.MODE === "production";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: isDev
+    ? import.meta.env.VITE_LOCAL_HOST
+    : import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
