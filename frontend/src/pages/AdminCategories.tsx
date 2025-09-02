cae2e9ca3d3e07ed45d2dd15ca1e4f89cb598625
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-09-02 15:40:02 +0800

    fix vercel compilation bug

--- frontend/src/pages/AdminCategories.tsx
+++ frontend/src/pages/AdminCategories.tsx
@@ -1,9 +1,7 @@
 import CreateCategoryModal from "@/components/modals/CreateCategoryModal";
 import CategoryPage from "@/components/tables/categories/CategoryPage";
-import { useGetCategories } from "@/hooks/useGetCategories";
 
 const AdminCategories = () => {
-  const { data } = useGetCategories();
   return (
     <div className="mx-5 mt-10">
       <div className="flex items-center justify-between">
